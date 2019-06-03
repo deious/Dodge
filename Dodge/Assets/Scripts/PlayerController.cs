a082bd08991e32bffe8d32581649fbba9c112021
deious <gkstjddlekd@naver.com>
2019-06-03 23:30:37 +0900

    Add Player Move
    - Add Player Move
    - Add Player Die

--- Dodge/Assets/Scripts/PlayerController.cs
+++ Dodge/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            PlayerRigidbody.AddForce(0f, 0f, Speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            PlayerRigidbody.AddForce(0f, 0f, -Speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            PlayerRigidbody.AddForce(Speed, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            PlayerRigidbody.AddForce(-Speed, 0f, 0f);
+        }
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
