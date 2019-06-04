7d263bbadc7a4399759af6f70249380a3c211442
deious <gkstjddlekd@naver.com>
2019-06-04 23:12:42 +0900

    Fix PlayerController
    - Fix Player's MoveMent

--- Dodge/Assets/Scripts/PlayerController.cs
+++ Dodge/Assets/Scripts/PlayerController.cs
@@ -4,36 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody PlayerRigidbody;
-    public float Speed = 8f;
+    private Rigidbody playerRigidbody;
+    public float speed = 8f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.UpArrow) == true)
+        /*if(Input.GetKey(KeyCode.UpArrow) == true)
         {
-            PlayerRigidbody.AddForce(0f, 0f, Speed);
+            playerRigidbody.AddForce(0f, 0f, Speed);
         }
 
         if (Input.GetKey(KeyCode.DownArrow) == true)
         {
-            PlayerRigidbody.AddForce(0f, 0f, -Speed);
+            playerRigidbody.AddForce(0f, 0f, -Speed);
         }
 
         if (Input.GetKey(KeyCode.RightArrow) == true)
         {
-            PlayerRigidbody.AddForce(Speed, 0f, 0f);
+            playerRigidbody.AddForce(Speed, 0f, 0f);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
-            PlayerRigidbody.AddForce(-Speed, 0f, 0f);
-        }
+            playerRigidbody.AddForce(-Speed, 0f, 0f);
+        }*/
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        playerRigidbody.velocity = newVelocity;
     }
 
     public void Die()
