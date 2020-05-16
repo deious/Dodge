6f46c1fc4f0a084ed40237bb0e20278384ef1912
deious <gkstjddlekd@naver.com>
2020-05-16 22:19:09 +0900

    Add BulletSpawner
    - Create a BulletSpawner to Fire Bullets

---
+++ Dodge/Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 3f;
+
+    Transform target;
+    float spawnRate;
+    float timeAfterSpawn;
+
+    void Start()
+    {
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+        
+        if(timeAfterSpawn >= spawnRate)
+        {
+            timeAfterSpawn = 0f;
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.transform.LookAt(target);
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
+    }
+}
