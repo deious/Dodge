5070298518c4e3d0960b5f749f22deafa148ef23
deious <gkstjddlekd@gmail.com>
2022-06-14 23:40:27 +0900

    이동속도 증가 스킬 추가

--- Dodge/Assets/Scripts/PlayerController.cs
+++ Dodge/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 8f;
     public Animator ani;
     public AnimatorControllerParameter anc;
+    public Image img;
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
@@ -28,6 +30,11 @@ public class PlayerController : MonoBehaviour
             ani.SetBool("RunCheck", false);
         else
             ani.SetBool("RunCheck", true);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            UseSteampack();
+        }
     }
 
     public void Die()
@@ -36,4 +43,33 @@ public class PlayerController : MonoBehaviour
         GameManager gameManager = FindObjectOfType<GameManager>();
         gameManager.EndGame();
     }
+
+    public void UseSteampack()
+    {
+        speed += 5;
+    }
+
+    /*IEnumerator CoolTime(int selected)
+    {
+        DungeonManager.Instance.characterLists[selected].totalSkillCheck = false;
+        DungeonManager.Instance.shieldCheck = false;
+        float timeCheck = 1f;
+        characterPrefabs[selected].GetComponent<Animator>().SetTrigger("Shield_t");
+        characterPrefabs[selected].transform.Find("Shield").gameObject.SetActive(true);
+        shieldCoolTime.transform.GetComponent<Image>().fillAmount = 1;
+        float temp = shieldCoolTime.transform.GetComponent<Image>().fillAmount / timeCheck;
+        shieldCoolTime.SetActive(true);
+        StartCoroutine(ShieldOff(timeCheck, selected));
+        while (timeCheck > 0)
+        {
+            shieldCoolTime.transform.GetComponent<Image>().fillAmount -= temp * 0.1f;
+            timeCheck -= 0.1f;
+            yield return new WaitForSeconds(0.1f);
+        }
+        characterPrefabs[selected].GetComponent<Animator>().SetBool("ShieldCheck", false);
+        shieldCoolTime.SetActive(false);
+
+        characterPrefabs[selected].GetComponent<Animator>().SetBool("ShieldCheck", true);
+        DungeonManager.Instance.shieldCheck = true;
+    }*/
 }
